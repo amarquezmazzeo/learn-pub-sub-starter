921a3d4171d3c28515704d7376386772095b4f08
Armando Marquez <armando.marquez.mz@gmail.com>
2026-01-30 19:22:45 -0500

    Adding rabitMQ connectivity and enabling sig interrupt to close the program

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	amqpConn := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(amqpConn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	fmt.Println("RabbitMQ connection stablished successfully!")
+
+	closeSig := make(chan os.Signal, 1)
+	signal.Notify(closeSig, os.Interrupt)
+	//<-closeSig
+
+	for sigg := range closeSig {
+		fmt.Printf("\nTerminating program: singal %s received\n", sigg)
+		os.Exit(1)
+	}
 }
